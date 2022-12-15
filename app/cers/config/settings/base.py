3c8907b90e6bfb85b399dc9d4f6009efac7f73d9
Koomin <bartoszkominko@gmail.com>
2022-12-15 18:10:09 +0100

    Add psycopg2 binary to req.

--- app/cers/config/settings/base.py
+++ app/cers/config/settings/base.py
@@ -34,7 +34,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 
 
 
-ALLOWED_HOSTS = get_env_variable('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [get_env_variable('ALLOWED_HOSTS')]
 
 # Application definition
 
