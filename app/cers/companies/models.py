7fae1ea4acffb564ade23f1710bffa407d55a350
Koomin <bartoszkominko@gmail.com>
2022-12-16 17:49:35 +0100

    Add department to companies.

--- app/cers/companies/models.py
+++ app/cers/companies/models.py
@@ -14,3 +14,16 @@ class Company(CersModel):
 
     def __str__(self):
         return self.name
+
+
+class Department(CersModel):
+    name = models.CharField(max_length=255, null=False, blank=False, verbose_name=_('name'))
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name=_('company'))
+
+    class Meta:
+        verbose_name = _('Department')
+        verbose_name_plural = _('Departments')
+        app_label = 'cers_auth'
+
+    def __str__(self):
+        return self.name
