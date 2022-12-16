29707c13dd3fa7bef27d42cfeb16116e668f661a
Koomin <bartoszkominko@gmail.com>
2022-12-16 17:52:31 +0100

    Add verbose name to company field.

--- app/cers/companies/models.py
+++ app/cers/companies/models.py
@@ -5,7 +5,7 @@ from cers.core.models import CersModel
 
 
 class Company(CersModel):
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False, verbose_name=_('name'))
 
     class Meta:
         verbose_name = _('Company')
