55f4f6e35e94ff25507a8ecbe685344b6a7fec9f
Koomin <bartoszkominko@gmail.com>
2023-01-04 16:40:53 +0100

    Add media url.

--- app/cers/config/urls.py
+++ app/cers/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from cers.core.admin import admin_site
 
@@ -25,4 +27,5 @@ urlpatterns = [
 ]
 
 urlpatterns += i18n_patterns(path('', include('cers.tickets.urls')),
-                             path('', admin_site.urls), prefix_default_language=False)
+                             path('', admin_site.urls), prefix_default_language=False),
+                             static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
