d6c084e0a970b8f8866fed1c87dc2e0e557c79f7
Koomin <bartoszkominko@gmail.com>
2023-01-19 18:42:00 +0100

    Changes on computer fields, remove bios password, ssd, mac_address.

--- app/cers/hardware/admin.py
+++ app/cers/hardware/admin.py
@@ -3,8 +3,8 @@ from cers.hardware.models import Computer
 
 
 class ComputerAdmin(CersModelAdmin):
-    list_display = ['name', 'model', 'mac_address', 'number', 'operating_system', 'office', 'bios_password',
-                    'bitlocker', 'backup', 'ssd', 'norton', 'synology_pass', 'comment', 'vpn', 'serial_number']
+    list_display = ['name', 'model', 'number', 'operating_system', 'office', 'bitlocker', 'backup', 'norton_installed',
+                    'synology_pass', 'comment', 'vpn', 'serial_number']
 
 
 admin_site.register(Computer, ComputerAdmin)
