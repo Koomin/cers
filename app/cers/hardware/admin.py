203c8a6760a34e59e26c1ea96c83df65994254e6
Koomin <bartoszkominko@gmail.com>
2023-01-27 11:35:03 +0100

    Create ComputerSet model.

--- app/cers/hardware/admin.py
+++ app/cers/hardware/admin.py
@@ -1,5 +1,6 @@
 from cers.core.admin import CersModelAdmin, admin_site
-from cers.hardware.models import Computer
+from cers.hardware.models import Computer, ComputerSet, Processor, HardDrive, Memory, PowerSupply, Motherboard, \
+    OperatingSystem, Manufacturer
 
 
 class ComputerAdmin(CersModelAdmin):
@@ -7,4 +8,21 @@ class ComputerAdmin(CersModelAdmin):
                     'synology_pass', 'comment', 'serial_number']
 
 
+class ComputerSetAdmin(CersModelAdmin):
+    list_display = ['company', 'model', 'serial_number', 'operating_system', 'processor', 'hard_drive', 'memory_ram',
+                    'power_supply', 'motherboard', 'date_of_sale', 'warranty']
+
+
+class ComponentAdmin(CersModelAdmin):
+    list_display = ['manufacturer', 'model']
+
+
 admin_site.register(Computer, ComputerAdmin)
+admin_site.register(ComputerSet, ComputerSetAdmin)
+admin_site.register(Processor, ComponentAdmin)
+admin_site.register(HardDrive, ComponentAdmin)
+admin_site.register(Memory, ComponentAdmin)
+admin_site.register(PowerSupply, ComponentAdmin)
+admin_site.register(Motherboard, ComponentAdmin)
+admin_site.register(OperatingSystem, CersModelAdmin)
+admin_site.register(Manufacturer, CersModelAdmin)
--- app/cers/hardware/models.py
+++ app/cers/hardware/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from cers.core.models import CersModel
-from companies.models import Company
+from cers.companies.models import Company
+
+
+class Manufacturer(CersModel):
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Manufacturer')
+        verbose_name_plural = _('Manufacturers')
 
 
 class OperatingSystem(CersModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Operating system')
+        verbose_name_plural = _('Operating systems')
 
 
 class Computer(CersModel):
@@ -26,40 +44,71 @@ class Computer(CersModel):
         verbose_name_plural = _('Computers')
 
 
-class Processor(CersModel):
-    pass
+class Component(CersModel):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f'{self.manufacturer.name} {self.model}'
 
 
-class HardDrive(CersModel):
-    pass
+class Processor(Component):
+    class Meta:
+        verbose_name = _('Processor')
+        verbose_name_plural = _('Processors')
 
 
-class Memory(CersModel):
-    pass
+class HardDrive(Component):
+    class Meta:
+        verbose_name = _('Hard Drive')
+        verbose_name_plural = _('Hard Drives')
 
 
-class PowerSupply(CersModel):
-    pass
+class Memory(Component):
+    class Meta:
+        verbose_name = _('Memory')
+        verbose_name_plural = _('Memories')
 
 
-class Motherboard(CersModel):
-    pass
+class PowerSupply(Component):
+    class Meta:
+        verbose_name = _('Power Supply')
+        verbose_name_plural = _('Power Supplies')
+
+
+class Motherboard(Component):
+    class Meta:
+        verbose_name = _('Motherboard')
+        verbose_name_plural = _('Motherboards')
 
 
 class ComputerSet(CersModel):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
-    model = models.CharField(max_length=255)
-    serial_number = models.CharField(max_length=255)
-    operating_system = models.ForeignKey(OperatingSystem, on_delete=models.CASCADE, null=True, blank=True)
-    processor = models.ForeignKey(Processor, on_delete=models.CASCADE)
-    processor_serial_number = models.CharField(max_length=255, null=True, blank=True)
-    hard_drive = models.ForeignKey(HardDrive, on_delete=models.CASCADE)
-    hard_drive_serial_number = models.CharField(max_length=255, null=True, blank=True)
-    memory_ram = models.ForeignKey(Memory, on_delete=models.CASCADE)
-    memory_ram_serial_number = models.CharField(max_length=255, null=True, blank=True)
-    power_supply = models.ForeignKey(PowerSupply, on_delete=models.CASCADE)
-    power_supply_serial_number = models.CharField(max_length=255, null=True, blank=True)
-    motherboard = models.ForeignKey(Motherboard, on_delete=models.CASCADE)
-    motherboard_serial_number = models.CharField(max_length=255, null=True, blank=True)
-    date_of_sale = models.DateField()
-    warranty = models.IntegerField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('Company'))
+    model = models.CharField(max_length=255, verbose_name=_('Model'))
+    serial_number = models.CharField(max_length=255, verbose_name=_('Serial number'))
+    operating_system = models.ForeignKey(OperatingSystem, on_delete=models.CASCADE, null=True, blank=True,
+                                         verbose_name=_('Operating system'))
+    processor = models.ForeignKey(Processor, on_delete=models.CASCADE, verbose_name=_('Processor'))
+    processor_serial_number = models.CharField(max_length=255, null=True, blank=True,
+                                               verbose_name=_('Processor serial number'))
+    hard_drive = models.ForeignKey(HardDrive, on_delete=models.CASCADE, verbose_name=_('Hard drive'))
+    hard_drive_serial_number = models.CharField(max_length=255, null=True, blank=True,
+                                                verbose_name=_('Hard drive serial number'))
+    memory_ram = models.ForeignKey(Memory, on_delete=models.CASCADE, verbose_name=_('Memory'))
+    memory_ram_serial_number = models.CharField(max_length=255, null=True, blank=True,
+                                                verbose_name=_('Memory serial number'))
+    power_supply = models.ForeignKey(PowerSupply, on_delete=models.CASCADE, verbose_name=_('Power supply'))
+    power_supply_serial_number = models.CharField(max_length=255, null=True, blank=True,
+                                                  verbose_name=_('Power supply serial number'))
+    motherboard = models.ForeignKey(Motherboard, on_delete=models.CASCADE, verbose_name=_('Motherboard'))
+    motherboard_serial_number = models.CharField(max_length=255, null=True, blank=True,
+                                                 verbose_name=_('Motherboard serial number'))
+    date_of_sale = models.DateField(verbose_name=_('Date of sale'))
+    warranty = models.IntegerField(verbose_name=_('Warranty'))
+
+    class Meta:
+        verbose_name = _('Computer set')
+        verbose_name_plural = _('Computer sets')
